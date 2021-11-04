be9e30a9bbce8ad9466c91e9053f8c5dec299bf6
Cody Raffensperger <cody.raffensperger@gmail.com>
2021-11-03 23:49:07 -0400

    Add anchor so the pin appears correctly

--- src/utils/MarkerIcon.ts
+++ src/utils/MarkerIcon.ts
@@ -4,6 +4,8 @@ import icon from "./leaflet/marker-icon.png";
 const markerIcon = new L.Icon({
   iconUrl: icon,
   iconRetinaUrl: icon,
+  iconSize: new L.Point(25, 41),
+  iconAnchor: [12.5, 41],
 });
 
 export { markerIcon };
